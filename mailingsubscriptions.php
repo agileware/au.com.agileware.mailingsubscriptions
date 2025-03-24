69e849cc6bdd7783f410cee8a8b1da6d573aab55
agileware-fj <agileware-fj@users.noreply.github.com>
2025-03-24 03:25:39 +0000

    CIVIBLD-287 [AUTO] civix upgraded
    Files changed:
    M	CRM/Mailingsubscriptions/Upgrader.php
    D	CRM/Mailingsubscriptions/Upgrader/Base.php
    M	info.xml
    M	mailingsubscriptions.civix.php
    M	mailingsubscriptions.php
    A	mixin/lib/civimix-schema@5.80.2/pathload.main.php
    A	mixin/lib/civimix-schema@5.80.2/src/AutomaticUpgrader.php
    A	mixin/lib/civimix-schema@5.80.2/src/CiviMixSchema.php
    A	mixin/lib/civimix-schema@5.80.2/src/DAO.php
    A	mixin/lib/civimix-schema@5.80.2/src/SchemaHelper.php
    A	mixin/lib/civimix-schema@5.80.2/src/SchemaHelperInterface.php
    A	mixin/lib/civimix-schema@5.80.2/src/SqlGenerator.php
    A	mixin/lib/pathload-0.php
    A	mixin/smarty-v2@1.0.3.mixin.php

--- mailingsubscriptions.php
+++ mailingsubscriptions.php
@@ -22,55 +22,10 @@ function mailingsubscriptions_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function mailingsubscriptions_civicrm_postInstall() {
-  _mailingsubscriptions_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function mailingsubscriptions_civicrm_uninstall() {
-  _mailingsubscriptions_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function mailingsubscriptions_civicrm_enable() {
   _mailingsubscriptions_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function mailingsubscriptions_civicrm_disable() {
-  _mailingsubscriptions_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function mailingsubscriptions_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _mailingsubscriptions_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function mailingsubscriptions_civicrm_entityTypes(&$entityTypes) {
-  _mailingsubscriptions_civix_civicrm_entityTypes($entityTypes);
 }
